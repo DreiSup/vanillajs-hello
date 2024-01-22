3b3d817359be8c176544f0a7b9da308b3cfd2986
DreiSup <153184780+DreiSup@users.noreply.github.com>
2024-01-22 11:50:32 +0000

    First commit

--- src/app.js
+++ src/app.js
@@ -1,11 +1,43 @@
-/* eslint-disable */
 import "bootstrap";
 import "./style.css";
 
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+window.onload = () => {
+  document.querySelector("#bttn").addEventListener("click", () => {
+    document.querySelector("#excuse").innerHTML = tellExcuse();
+  });
+  document.querySelector("#excuse").innerHTML = tellExcuse();
+};
+let tellExcuse = () => {
+  let who = ["My grandma", "A cow", "My cousin", "My neighbor"];
+  let action = ["ate", "peed on", "broke", "burned up"];
+  let what = [
+    "my homework",
+    "my pc",
+    "my monitor",
+    "the electric cables of my house"
+  ];
+  let when = [
+    "quarter a second ago",
+    "last decade",
+    "few months ago",
+    "yesterday"
+  ];
+
+  let whoIndx = Math.floor(Math.random() * who.length);
+  let actionIndx = Math.floor(Math.random() * action.length);
+  let whatIndx = Math.floor(Math.random() * what.length);
+  let whenIndx = Math.floor(Math.random() * when.length);
+
+  return (
+    who[whoIndx] +
+    " " +
+    action[actionIndx] +
+    " " +
+    what[whatIndx] +
+    " " +
+    when[whenIndx]
+  );
 };
